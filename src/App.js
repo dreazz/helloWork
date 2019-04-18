4912adcede73da251f067d0e0eb0f79df8370301
dreazz <agustinsilveira.96@gmail.com>
2019-04-18 19:55:28 +0200

    job cards with data create

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import JobCard from './components/jobCard/JobCard'
+import JobCard from './components/jobCard/JobCard';
+import jobData from './jobsApril.json' ;
 import './App.css';
 
 class App extends Component {
-  state = {
-    jobArray: [
-      {name:"web developer",
-       description:"this is an example of a job"   
-    }]
-  }
   render() {
     return (
       <div className="main">
         <div className="header">
           <h1>Say Hi, to your new job <span role="image">👋</span></h1>
         </div>
-        <div class="body">
-        {this.state.jobArray.map((job)=>{
-          return <JobCard title={job.name} description={job.description}/>
+        <div className="body">
+        {jobData.map((job,i)=>{
+          return <JobCard key={job.Company + `${i}`}position={job.Position} company={job.Company} status={job.Status} date={job.Date} link={job.LinkUrl}/>
         })}
          
         </div>
--- src/components/jobCard/JobCard.js
+++ src/components/jobCard/JobCard.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
+import "./jobCard.css"
 
 export default class jobCard extends Component {
   render() {
     return (
       <div className="job-card">
-        <h1>{this.props.title}</h1>
-        <p>{this.props.description}</p>
-        <button>See more</button>
+        <h1>{this.props.position}</h1>
+        <p><strong>Company: </strong>{this.props.company}</p>
+        <p><strong>status: </strong>{this.props.status}</p>
+        <p><strong>date: </strong>{this.props.date}</p>
+        <button><a href={this.props.link}>See more</a></button>
       </div>
     )
   }
