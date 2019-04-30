b1e6e3078716fb65f2df19e743184f4e0d2571d1
dreazz <agustinsilveira.96@gmail.com>
2019-04-30 12:07:14 +0200

    change form beacuse the old one didn't work

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,21 +1,47 @@
 import React, { Component } from 'react'
+const encode = (data) => {
+  return Object.keys(data)
+      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+      .join("&");
+}
 
 export default class Form extends Component {
+  state={
+    name:"",
+    description:"",
+  }
+
+  handleSubmit = e => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", ...this.state })
+    })
+      .then(() => alert("Success!"))
+      .catch(error => alert(error));
+
+    e.preventDefault();
+  };
+
+  handleChange = e => this.setState({ [e.target.name]: e.target.value });
   render() {
-    return (
-      <div>
-        <form name="contact" netlify>
+    const { name, description } = this.state;
+      return (
+        <form onSubmit={this.handleSubmit}>
           <p>
-            <label>Name <input type="text" name="name" /></label>
+            <label>
+              Your Name: <input type="text" name="name" value={name} onChange={this.handleChange} />
+            </label>
           </p>
           <p>
-            <label>Email <input type="email" name="email" /></label>
+            <label>
+              Description: <input type="text" name="description" value={description} onChange={this.handleChange} />
+            </label>
           </p>
           <p>
             <button type="submit">Send</button>
           </p>
         </form>
-      </div>
-    )
+      );
   }
 }
